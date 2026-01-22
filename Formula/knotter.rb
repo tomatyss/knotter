1c512f34de62d188394826f3b7faf1d5f4a6bf9e
Ivan Iufriakov <tomatyss@gmail.com>
2026-01-21 21:35:07 -0500

    Update Homebrew formula for v0.1.3

--- Formula/knotter.rb
+++ Formula/knotter.rb
@@ -1,8 +1,8 @@
 class Knotter < Formula
   desc "Terminal-first personal CRM and friendship tracker"
   homepage "https://github.com/tomatyss/knotter"
-  url "https://github.com/tomatyss/knotter/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "6b57486e0e7949e5988880361fb228f42322bcbe8dbca78b3006d5ff449a9e98"
+  url "https://github.com/tomatyss/knotter/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "e9651bc0b020a5866fd7abb3f42070d218653b3ec7de5e5f7ba3d04d89456cb9"
   license "MIT"
 
   depends_on "rust" => :build
